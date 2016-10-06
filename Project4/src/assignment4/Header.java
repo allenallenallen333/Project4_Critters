a517a6e762e0d78f116b2fd904aa18f4a5cf2589
allenallenallen333 <allen3@utexas.edu>
2016-10-06 10:00:20 -0500

    Starter Project and Files

---
+++ Project4/src/assignment4/Header.java
@@ -0,0 +1,13 @@
+package assignment4;
+/* CRITTERS <Main.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Jia-luen Yang
+ * JY8435
+ * 16455
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: 0
+ * Fall 2016
+ */
